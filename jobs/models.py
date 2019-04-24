0fd5e61b52b186cf0e7f6c5d82177594af6f098f
navid <navid.rashik@gmail.com>
2019-04-25 00:53:49 +0600

    adding Jobs to admin pannel and detabase shift to postgres

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    images = models.ImageField( upload_to='images/')
+    summery = models.CharField(max_length=150)

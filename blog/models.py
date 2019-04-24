929a192c9ad7b6b76ef1107190c7b80192f1476e
navid <navid.rashik@gmail.com>
2019-04-25 01:44:02 +0600

    blog implemented

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField( max_length=150)
+    publication_date = models.DateField("Date")
+    body_text = models.TextField(("input text"))
+    image = models.ImageField(("image input"), upload_to='images/', height_field=None, width_field=None, max_length=None)

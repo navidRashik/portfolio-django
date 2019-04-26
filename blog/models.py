3f94eb282bb8019646a5037d02a2696462baa2ed
navid <navid.rashik@gmail.com>
2019-04-27 00:46:23 +0600

    blog detail view added partially

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,7 @@ class Blog(models.Model):
     publication_date = models.DateField("Date")
     body_text = models.TextField(("input text"))
     image = models.ImageField(("image input"), upload_to='images/', height_field=None, width_field=None, max_length=None)
+
+
+    def summary(self):
+        return self.body_text[:150]
